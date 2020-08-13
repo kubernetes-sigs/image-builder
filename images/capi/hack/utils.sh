da98362356ec3c2ba5bc6b977ba9f0d3674535c4
Travis Rhoden <trhoden@vmware.com>
2020-08-12 21:19:07 -0600

    Fix ensure Py3, Pip3, and azure-cli
    This patch de-dedups logic for making sure that Python3 and pip3 are
    installed. It also corrects a corner case where pip3 could be installed
    to the user local Python bin dir but that dir was not in path.
    
    And finally, for checking that azure-cli was installed (which goes to
    the user local Python bin dir), the wrong command name was used. It was
    looking for azure-cli when it should look for az.

--- images/capi/hack/utils.sh
+++ images/capi/hack/utils.sh
@@ -67,3 +67,28 @@ get_shasum() {
   fi
   echo "$present_shasum"
 }
+
+ensure_py3_bin() {
+  # If given executable is not available, the user Python bin dir is not in path
+  # This function assumes the executable to be checked was installed with
+  # pip3 install --user ...
+  if ! command -v "${1}" >/dev/null 2>&1; then
+    echo "User's Python3 binary directory must be in \$PATH" 1>&2
+    echo "This is likely \$HOME/.local/bin" 1>&2
+    echo "\$PATH is currently: $PATH" 1>&2
+    exit 1
+  fi
+}
+
+ensure_py3() {
+  if ! command -v python3 >/dev/null 2>&1; then
+    echo "python3 binary must be in \$PATH" 1>&2
+    exit 1
+  fi
+  if ! command -v pip3 >/dev/null 2>&1; then
+    curl -SsL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py --user
+    rm -f get-pip.py
+    ensure_py3_bin pip3
+  fi
+}
