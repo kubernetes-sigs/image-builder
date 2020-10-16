c9632145b19cf444502bf3bad8e8c76423141eff
Cecile Robert-Michon <cerobert@microsoft.com>
2020-10-16 12:26:50 -0700

    Make default Azure storage account name unique and check name before creating

--- images/capi/packer/azure/scripts/init-vhd.sh
+++ images/capi/packer/azure/scripts/init-vhd.sh
@@ -10,5 +10,7 @@ export RESOURCE_GROUP_NAME="${RESOURCE_GROUP_NAME:-cluster-api-images}"
 export AZURE_LOCATION="${AZURE_LOCATION:-southcentralus}"
 az group create -n ${RESOURCE_GROUP_NAME} -l ${AZURE_LOCATION} --tags ${TAGS}
 CREATE_TIME="$(date +%s)"
-export STORAGE_ACCOUNT_NAME="capi${CREATE_TIME}"
+RANDOM_SUFFIX="$(head /dev/urandom | LC_ALL=C tr -dc a-z | head -c 4 ; echo '')"
+export STORAGE_ACCOUNT_NAME="capi${CREATE_TIME}${RANDOM_SUFFIX}"
+az storage account check-name --name ${STORAGE_ACCOUNT_NAME}
 az storage account create -n ${STORAGE_ACCOUNT_NAME} -g ${RESOURCE_GROUP_NAME}
