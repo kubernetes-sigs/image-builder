b3ff7253e14094bae1ad9d5692a2704feb0528df
Cecile Robert-Michon <cerobert@microsoft.com>
2020-11-05 14:30:12 -0800

    Add centos-7 SIG definition

--- images/capi/packer/azure/scripts/init-sig.sh
+++ images/capi/packer/azure/scripts/init-sig.sh
@@ -9,7 +9,9 @@ eval "$tracestate"
 export RESOURCE_GROUP_NAME="${RESOURCE_GROUP_NAME:-cluster-api-images}"
 export AZURE_LOCATION="${AZURE_LOCATION:-southcentralus}"
 az group create -n ${RESOURCE_GROUP_NAME} -l ${AZURE_LOCATION}
-export GALLERY_NAME="${GALLERY_NAME:-ClusterAPI}"
+CREATE_TIME="$(date +%s)"
+RANDOM_SUFFIX="$(head /dev/urandom | LC_ALL=C tr -dc a-z | head -c 4 ; echo '')"
+export GALLERY_NAME="${GALLERY_NAME:-ClusterAPI${CREATE_TIME}${RANDOM_SUFFIX}}"
 az sig create --resource-group ${RESOURCE_GROUP_NAME} --gallery-name ${GALLERY_NAME}
 az sig image-definition create \
    --resource-group ${RESOURCE_GROUP_NAME} \
@@ -26,4 +28,12 @@ az sig image-definition create \
    --publisher capz \
    --offer capz-demo \
    --sku 20_04-lts \
+   --os-type Linux
+az sig image-definition create \
+   --resource-group ${RESOURCE_GROUP_NAME} \
+   --gallery-name ${GALLERY_NAME} \
+   --gallery-image-definition capi-centos-7.7 \
+   --publisher capz \
+   --offer capz-demo \
+   --sku centos-7.7 \
    --os-type Linux
