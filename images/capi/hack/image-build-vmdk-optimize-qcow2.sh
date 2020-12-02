b7e854f9c38fa8ed1d32fcd898f8c288944f0666
SEAN SCHNEEWEISS <sean.schneeweiss@daimler.com>
2020-12-02 13:15:06 +0100

    dhc/caas: increase /var/log partition to 2GB
    The partition for /var/log is mostly used to 98%. As there is enough
    space available, the partition is increased to 2GB.
    The build directory is adapted to the upstream used k8s v1.17.11.

--- images/capi/hack/image-build-vmdk-optimize-qcow2.sh
+++ images/capi/hack/image-build-vmdk-optimize-qcow2.sh
@@ -3,7 +3,7 @@
 BUILD_DIR=$1
 
 echo "Converting qcow2 to streamOptimized vmdk"
-qemu-img convert -f qcow2 -O vmdk -o subformat=streamOptimized ${BUILD_DIR}/qemu-kube-v1.17.3 ${BUILD_DIR}/qemu-kube-v1.17.3.vmdk
+qemu-img convert -f qcow2 -O vmdk -o subformat=streamOptimized "${BUILD_DIR}"/qemu-kube-v1.17.11 "${BUILD_DIR}"/qemu-kube-v1.17.11.vmdk
 
 echo "Compressing qcow2"
-qemu-img convert -f qcow2 -O qcow2 -c ${BUILD_DIR}/qemu-kube-v1.17.3 ${BUILD_DIR}/qemu-kube-v1.17.3.qcow2
+qemu-img convert -f qcow2 -O qcow2 -c "${BUILD_DIR}"/qemu-kube-v1.17.11 "${BUILD_DIR}"/qemu-kube-v1.17.11.qcow2
