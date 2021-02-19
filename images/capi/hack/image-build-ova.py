ab6b1898141b26c55d61091ac381555c9c8f90c8
Sam Mirza <mirzasa@vmware.com>
2021-02-19 14:18:59 -0800

    Updates `ubuntu-64` OS type to `ubuntu64Guest`
    `ubuntu64Guest` is a supported OS type

--- images/capi/hack/image-build-ova.py
+++ images/capi/hack/image-build-ova.py
@@ -117,7 +117,7 @@ def main():
     OS_id_map = {"vmware-photon-64": {"id": "36", "version": "", "type": "vmwarePhoton64Guest"},
                  "centos7-64": {"id": "107", "version": "7", "type": "centos7-64"},
                  "rhel7-64": {"id": "80", "version": "7", "type": "rhel7_64guest"},
-                 "ubuntu-64": {"id": "94", "version": "", "type": "ubuntu-64"},
+                 "ubuntu-64": {"id": "94", "version": "", "type": "ubuntu64Guest"},
                  "Windows2019Server-64": {"id": "112", "version": "", "type": "windows9srv-64"},
                  "Windows2004Server-64": {"id": "112", "version": "", "type": "windows9srv-64"}}
 
