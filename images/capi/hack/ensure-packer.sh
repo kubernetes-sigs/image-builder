bd0fa3e6e9b48c931bdd4384d156bdb1644d4077
Christian Berendt <berendt@23technologies.cloud>
2021-03-28 14:24:54 +0200

    Bump packer-provisioner-goss to 3.0.3 & packer to 1.7.2
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- images/capi/hack/ensure-packer.sh
+++ images/capi/hack/ensure-packer.sh
@@ -20,7 +20,7 @@ set -o pipefail
 
 [[ -n ${DEBUG:-} ]] && set -o xtrace
 
-_version="1.6.6"
+_version="1.7.2"
 
 # Change directories to the parent directory of the one in which this
 # script is located.
