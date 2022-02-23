35f631f243118fa58b143167eed9160a30387fdb
SEAN SCHNEEWEISS <sean.schneeweiss@daimler.com>
2022-02-23 05:57:41 +0100

    images/capi: move custom files to t1

--- images/capi/hack/image-build-vmdk-optimize-qcow2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-BUILD_DIR=$1
-
-echo "Converting qcow2 to streamOptimized vmdk"
-qemu-img convert -f qcow2 -O vmdk -o subformat=streamOptimized "${BUILD_DIR}"/qemu-kube-v1.17.11 "${BUILD_DIR}"/qemu-kube-v1.17.11.vmdk
-
-echo "Compressing qcow2"
-qemu-img convert -f qcow2 -O qcow2 -c "${BUILD_DIR}"/qemu-kube-v1.17.11 "${BUILD_DIR}"/qemu-kube-v1.17.11.qcow2
