f42becdf066eb7264398b6cece99c74f29d35e0f
Marcus Noble <github@marcusnoble.co.uk>
2022-10-11 10:49:32 +0100

    Bump Ansible to v2.13.4
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- images/capi/hack/ensure-ansible.sh
+++ images/capi/hack/ensure-ansible.sh
@@ -22,7 +22,7 @@ set -o pipefail
 
 source hack/utils.sh
 
-_version="2.11.5"
+_version="${ANSIBLE_VERSION:-2.13.4}"
 
 # Change directories to the parent directory of the one in which this
 # script is located.
