e8c53cb8e12c56812aa644e955e8c975fd6de236
zylxjtu <zhang.yuanliang@hotmail.com>
2025-04-23 22:57:03 +0000

    Try to address the cloudbase init

---
+++ env.sh
@@ -0,0 +1,4 @@
+  export AZURE_LOCATION="uksouth"
+  export AZURE_CLIENT_ID="aa0caef4-31da-4426-9908-f75af27075c8"
+  export AZURE_SUBSCRIPTION_ID="1c8c4edc-b188-4199-9580-3173033d75f7"
+  export USE_AZURE_CLI_AUTH=true
