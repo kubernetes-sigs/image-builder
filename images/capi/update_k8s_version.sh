f3fd45afee449eb93bc21a40a5ec000d0444a364
Vigneshwar Rajkumar <vigneshwar.rajkumar@ditto.com>
2025-06-03 15:56:44 -0400

    Add GCP AMI build workflow
    Adds a GHA workflow job to Build GCP images
    Changes to update_k8s_version.sh to add support for GCP
    Renamed make_ami_public.sh to make_images_public.sh to add support for GCP
    Consolidate env

--- images/capi/update_k8s_version.sh
+++ images/capi/update_k8s_version.sh
@@ -1,26 +1,58 @@
 #!/bin/sh
 
-UBUNTU_FILE=$(find ./images/capi/packer -name "ubuntu-2404.json" | head -n 1)
-
-if [ -z "$UBUNTU_FILE" ]; then
-    echo "Error: ubuntu-2404.json file could not be found."
-    exit 1
-fi
-
-echo "Using ubuntu-2404.json at $UBUNTU_FILE"
-
 K8S_VERSION="${K8S_VERSION:-1.30.10}" # Default to 1.30.10 if not provided
 
-echo "Adding kubernetes_semver: $K8S_VERSION to $UBUNTU_FILE"
+CLOUD="${CLOUD:-aws}"
 
-jq --arg k8s_version "$K8S_VERSION" '.kubernetes_semver = $k8s_version' "$UBUNTU_FILE" > "$UBUNTU_FILE.tmp" && mv "$UBUNTU_FILE.tmp" "$UBUNTU_FILE"
+case "$CLOUD" in
+  aws)
+    # AWS-specific block to update AWS packer file
+    AWS_FILE=$(find ./images/capi/packer/ami -name "ubuntu-2404.json" | head -n 1)
 
-AWS_PROFILE_ENV="${AWS_PROFILE}"
-AWS_PROFILE_FILE=$(jq -r '.aws_profile' "$UBUNTU_FILE")
+    if [ -z "$AWS_FILE" ]; then
+        echo "Error: ubuntu-2404.json file could not be found."
+        exit 1
+    fi
 
-if [ "$AWS_PROFILE_ENV" != "$AWS_PROFILE_FILE" ]; then
-    echo "AWS profile mismatch. Updating aws_profile in $UBUNTU_FILE to match $AWS_PROFILE_ENV."
-    jq --arg aws_profile "$AWS_PROFILE_ENV" '.aws_profile = $aws_profile' "$UBUNTU_FILE" > "$UBUNTU_FILE.tmp" && mv "$UBUNTU_FILE.tmp" "$UBUNTU_FILE"
-else
-    echo "AWS profile matches: $AWS_PROFILE_ENV"
-fi
+    echo "Using ubuntu-2404.json at $AWS_FILE"
+    echo "Adding kubernetes_semver: $K8S_VERSION to $AWS_FILE"
+
+    jq --arg k8s_version "$K8S_VERSION" '.kubernetes_semver = $k8s_version' "$AWS_FILE" > "$AWS_FILE.tmp" && mv "$AWS_FILE.tmp" "$AWS_FILE"
+
+    AWS_PROFILE_ENV="${AWS_PROFILE}"
+    AWS_PROFILE_FILE=$(jq -r '.aws_profile' "$AWS_FILE")
+
+    if [ "$AWS_PROFILE_ENV" != "$AWS_PROFILE_FILE" ]; then
+        echo "AWS profile mismatch. Updating aws_profile in $AWS_FILE to match $AWS_PROFILE_ENV."
+        jq --arg aws_profile "$AWS_PROFILE_ENV" '.aws_profile = $aws_profile' "$AWS_FILE" > "$AWS_FILE.tmp" && mv "$AWS_FILE.tmp" "$AWS_FILE"
+    else
+        echo "AWS profile matches: $AWS_PROFILE_ENV"
+    fi
+    ;;
+  gcp)
+    # GCP-specific block to update GCP packer file
+    GCP_FILE=$(find ./images/capi/packer/gce -name "ubuntu-2404.json" | head -n 1)
+    if [ -z "$GCP_FILE" ]; then
+        echo "Error: GCP ubuntu-2404.json file could not be found."
+        exit 1
+    fi
+    echo "Using GCP ubuntu-2404.json at $GCP_FILE"
+    echo "Adding kubernetes_semver: $K8S_VERSION to $GCP_FILE"
+    jq --arg k8s_version "$K8S_VERSION" '.kubernetes_semver = $k8s_version' "$GCP_FILE" > "$GCP_FILE.tmp" && mv "$GCP_FILE.tmp" "$GCP_FILE"
+    ;;
+  azure)
+    # Azure-specific block to update Azure packer file
+    AZURE_FILE=$(find ./images/capi/packer/azure -name "ubuntu-2404.json" | head -n 1)
+    if [ -z "$AZURE_FILE" ]; then
+        echo "Error: Azure ubuntu-2404.json file could not be found."
+        exit 1
+    fi
+    echo "Using Azure ubuntu-2404.json at $AZURE_FILE"
+    echo "Adding kubernetes_semver: $K8S_VERSION to $AZURE_FILE"
+    jq --arg k8s_version "$K8S_VERSION" '.kubernetes_semver = $k8s_version' "$AZURE_FILE" > "$AZURE_FILE.tmp" && mv "$AZURE_FILE.tmp" "$AZURE_FILE"
+    ;;
+  *)
+    echo "Unknown CLOUD value: $CLOUD"
+    exit 1
+    ;;
+esac
