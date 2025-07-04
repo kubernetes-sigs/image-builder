b2093ef346a73251cdb3fd0b1955279df24a474e
Marcus Noble <github@marcusnoble.co.uk>
2025-07-04 09:40:01 +0100

    Updated Azure CI tests to automatically detect targets
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- images/capi/azure_targets.sh
+++ images/capi/azure_targets.sh
@@ -1,8 +1,4 @@
 VHD_TARGETS="ubuntu-2004 ubuntu-2204 ubuntu-2404 azurelinux-3 rhel-8 windows-2019-containerd windows-2022-containerd"
-VHD_CI_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 windows-2022-containerd"
 SIG_TARGETS="ubuntu-2004 ubuntu-2204 ubuntu-2404 azurelinux-3 rhel-8 windows-2019-containerd windows-2022-containerd windows-2025-containerd flatcar"
-SIG_CI_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 windows-2022-containerd windows-2025-containerd flatcar"
 SIG_GEN2_TARGETS="ubuntu-2004 ubuntu-2204 ubuntu-2404 azurelinux-3 flatcar"
-SIG_GEN2_CI_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 flatcar"
 SIG_CVM_TARGETS="ubuntu-2004 ubuntu-2204 ubuntu-2404 windows-2019-containerd windows-2022-containerd"
-SIG_CVM_CI_TARGETS="ubuntu-2204 ubuntu-2404 windows-2022-containerd"
