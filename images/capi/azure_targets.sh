11801c6556337941a45da1e4693785c96f906cf2
Alex Benn <alexbenn@microsoft.com>
2025-10-21 20:35:07 +0000

    add vars file for using the HPC image. Add support for arm64. Doesn't work yet.

--- images/capi/azure_targets.sh
+++ images/capi/azure_targets.sh
@@ -1,4 +1,5 @@
 VHD_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 rhel-8 windows-2019-containerd windows-2022-containerd"
 SIG_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 rhel-8 windows-2019-containerd windows-2022-containerd windows-2025-containerd flatcar"
-SIG_GEN2_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 flatcar"
+SIG_GEN2_TARGETS="ubuntu-2204 ubuntu-2404 ubuntu-2404-arm64 azurelinux-3 flatcar"
+SIG_ARM64_GEN2_TARGETS="ubuntu-2404-arm64"
 SIG_CVM_TARGETS="ubuntu-2204 ubuntu-2404 windows-2019-containerd windows-2022-containerd"
