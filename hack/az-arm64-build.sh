dc0d1a232f96b92c8557094204d7275344a254bf
Alex Benn <alexbenn@microsoft.com>
2026-01-06 14:43:38 +0000

    add hack script for boostrap

---
+++ hack/az-arm64-build.sh
@@ -0,0 +1,17 @@
+USE_AZURE_CLI_AUTH=true
+AZURE_SUBSCRIPTION_ID=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
+AZURE_LOCATION=centralus
+RESOURCE_GROUP_NAME=alexbenn-test-arm64-image-build
+DATE=$(date +%Y%m%d)
+SEQNUM=01
+SEQNUM_FILE=/tmp/az-arm64-build-seqnum.txt
+if [ -f $SEQNUM_FILE ]; then
+    SEQNUM=$(cat $SEQNUM_FILE)
+    SEQNUM=$((10#$SEQNUM + 1))
+    if [ $SEQNUM -lt 10 ]; then
+        SEQNUM="0$SEQNUM"
+    fi
+fi
+echo $SEQNUM > $SEQNUM_FILE
+
+make build-azure-sig-ubuntu-2404-arm64-gen2 2>&1 | tee /tmp/build-azure-sig-ubuntu-2404-arm64-gen2-$DATE-$SEQNUM.out
