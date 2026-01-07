c686a2fea07ae3bdd7ee71c065c710a153d68172
Alex Benn <alex.benn.is@gmail.com>
2026-01-07 09:43:27 -0500

    fix: reset seqno each day. export vars needed deeper in the makefile stack.

--- hack/az-arm64-build.sh
+++ hack/az-arm64-build.sh
@@ -1,10 +1,11 @@
-USE_AZURE_CLI_AUTH=true
-AZURE_SUBSCRIPTION_ID=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
-AZURE_LOCATION=centralus
-RESOURCE_GROUP_NAME=alexbenn-test-arm64-image-build
+export USE_AZURE_CLI_AUTH=true
+export AZURE_SUBSCRIPTION_ID=8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8
+export AZURE_LOCATION=centralus
+export RESOURCE_GROUP_NAME=alexbenn-test-arm64-image-build
+
 DATE=$(date +%Y%m%d)
 SEQNUM=01
-SEQNUM_FILE=/tmp/az-arm64-build-seqnum.txt
+SEQNUM_FILE=/tmp/az-arm64-build-seqnum-$DATE.txt
 if [ -f $SEQNUM_FILE ]; then
     SEQNUM=$(cat $SEQNUM_FILE)
     SEQNUM=$((10#$SEQNUM + 1))
