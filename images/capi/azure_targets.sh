7846ac055b681a2b33e4813ea34736e56d345156
Matt Boersma <Matt.Boersma@microsoft.com>
2026-05-14 10:40:58 -0600

    Add Azure Linux 4 Gen2 Azure SIG target

--- images/capi/azure_targets.sh
+++ images/capi/azure_targets.sh
@@ -1,4 +1,4 @@
 VHD_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 windows-2022-containerd"
 SIG_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 windows-2022-containerd windows-2025-containerd flatcar"
-SIG_GEN2_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 flatcar"
+SIG_GEN2_TARGETS="ubuntu-2204 ubuntu-2404 azurelinux-3 azurelinux-4 flatcar"
 SIG_CVM_TARGETS="ubuntu-2204 ubuntu-2404 windows-2022-containerd"
